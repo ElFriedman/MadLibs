ab42b607519e7a130d555c05e3afffb0159e89ce
Eli Friedman <elifriedman317@gmail.com>
2020-08-25 13:26:51 -0700

    Adverbs

--- MadLibs.java
+++ MadLibs.java
@@ -20,7 +20,11 @@ public class MadLibs {
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 		
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
-
+		BufferedReader adverbReader = new BufferedReader(adverbs.txt);
+		while (BufferedReader.ready()) {
+			adverbs.add(BufferedReader.readline());
+		}
+		br.close)()
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
